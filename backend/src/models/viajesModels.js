3bdc04e8e05ba9d73dc311b8247fabdd92942d9a
aguusoleti <agustin_soleti@hotmail.com>
2023-08-19 17:07:29 -0300

    fix model viajes

--- backend/src/models/viajesModels.js
+++ backend/src/models/viajesModels.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const viajeSchema = new mongoose.Schema({
-  origen: String,
-  destino: String,
-  fecha: Date,
+  origen: {
+    type: String,
+    required: true,
+  },
+  destino: {
+    type: String,
+    required: true,
+  },
+  fecha: {
+    type: Date,
+    required: true,
+  },
   id_conductor: { type: mongoose.Schema.Types.ObjectId, ref: 'Conductor' },
   id_usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
