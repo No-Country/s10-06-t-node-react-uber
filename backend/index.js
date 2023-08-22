bdd5c4ecf58c98d0fcc2107da7c91238418db16a
Luca Vitorino <valentinvitorimo28@gmail.com>
2023-08-22 16:50:12 -0300

    config cors

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,20 @@ import dotenv from 'dotenv';
 import server from './src/routes/serverRoute.js';
 import { initDBConnection } from './src/data/dbConnection.js';
 import login from './src/routes/loginRoute.js';
-
+import cors from 'cors';
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: FRONTEND_URL,
+  })
+);
 
 app.use('/', server);
 app.use(express.json());
