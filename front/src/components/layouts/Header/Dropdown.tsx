896e82c7b5d84162e92e24b4c37c814fa1d0d00c
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-08-23 12:32:47 -0500

    feat: Add dropdown options to Header Dropdown component

--- front/src/components/layouts/Header/Dropdown.tsx
+++ front/src/components/layouts/Header/Dropdown.tsx
@@ -1,24 +1,87 @@
 import { useState, type FC } from 'react'
 
 import { HiOutlineChevronDown } from 'react-icons/hi'
+import { DropdownChip } from './DropdownChip'
 
 export const Dropdown: FC = () => {
   const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ): void => {
+    e.target === e.currentTarget && setIsOpen(!isOpen)
+  }
+
   return (
-    <div
-      className='flex cursor-pointer items-center gap-2 rounded-full px-2 py-1 duration-300 hover:bg-white hover:bg-opacity-10'
-      onClick={() => {
-        setIsOpen(!isOpen)
-      }}
-    >
-      Más{' '}
-      <HiOutlineChevronDown
-        className='text-lg'
+    <>
+      <div
+        className='rounded-f relative flex cursor-pointer items-center gap-2'
+        onClick={handleClick}
+      >
+        Más{' '}
+        <HiOutlineChevronDown
+          className='text-lg'
+          style={{
+            transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+            transition: 'transform 0.2s ease',
+          }}
+          onClick={handleClick}
+        />
+      </div>
+      <div
+        className={`min-h-20 absolute left-28 top-[73px] z-10 flex flex-col gap-4 overflow-hidden rounded-b-md bg-white p-4 text-center text-neutral-400
+            ${isOpen ? 'max-h-[400px] opacity-100' : 'max-h-0 opacity-0'}
+          `}
         style={{
-          transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
-          transition: 'transform 0.2s ease',
+          transition: 'max-height 0.4s ease-in-out, opacity 0.4s',
         }}
-      />
-    </div>
+      >
+        {dropdownOptions.map(({ text, link }) => (
+          <DropdownChip key={text} link={link}>
+            {text}
+          </DropdownChip>
+        ))}
+      </div>
+    </>
   )
 }
+
+interface DropdownOptions {
+  text: string
+  link: string
+}
+
+const dropdownOptions: DropdownOptions[] = [
+  {
+    text: 'About us',
+    link: '/',
+  },
+  {
+    text: 'Our offerings',
+    link: '/',
+  },
+  {
+    text: 'How Uber Works',
+    link: '/',
+  },
+  {
+    text: 'Global citizienship',
+    link: '/',
+  },
+  {
+    text: 'Newsroom',
+    link: '/',
+  },
+  {
+    text: 'Investor relations',
+    link: '/',
+  },
+  {
+    text: 'Blog',
+    link: '/',
+  },
+  {
+    text: 'Carrers',
+    link: '/',
+  },
+]
