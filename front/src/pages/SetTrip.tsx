f71e3b0ef8761ea96723283cf0b554a9ec744eae
Victor Ayala <vickyflow6@gmail.com>
2023-08-25 16:45:02 -0400

    feat: Crea componente Input
    Este componente, se encarga de dar los estilos correspondientes a los inputs de la página SetTrip.

--- front/src/pages/SetTrip.tsx
+++ front/src/pages/SetTrip.tsx
@@ -1,23 +1,33 @@
+import Container from "../components/layouts/Container" 
 import { BiArrowBack } from "react-icons/bi"
 
-const SetTrip = () => {
+const SetTrip: () => React.ReactNode = () => {
     return (
-        <main>
-            <h2 className="text-18 flex"><BiArrowBack /> Solicitar un viaje</h2>
-            <form>
-                <ul>
-                    <li><input type="text" placeholder="¿De dónde salís?" /></li>
-                    <li><input type="text" placeholder="¿A dónde te llevamos?" /></li>
-                </ul>
-            </form>
-            <div>
-                <h5>Recientes</h5>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </main>
+        <Container>
+            <main className="text-20">
+                <h2 className="flex"><BiArrowBack /> Solicitar un viaje</h2>
+                <form>
+                    <ul>
+                        <li><Input className="mb-5" inputType="text" inputPlaceholder="¿De dónde salís?" /></li>
+                        <li><Input inputType="text" inputPlaceholder="¿A dónde te llevamos?" /></li>
+                    </ul>
+                </form>
+                <div>
+                    <h5>Recientes</h5>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </main>
+        </Container>
     );
+
+    function Input({ inputType, inputPlaceholder, className = "" }: { inputType: string, inputPlaceholder: string, className?: string }) {
+        const nameClass = `w-full rounded-full p-[6px] border-solid border-dark border-2 placeholder-dark ${className}`
+        return (
+            <input className={nameClass} type={inputType} placeholder={inputPlaceholder} /> 
+        );
+    }
 }
 
 export default SetTrip
