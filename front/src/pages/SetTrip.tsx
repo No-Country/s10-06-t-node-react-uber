9ece920c2294779e3f547a6fd8115f590b6c1b19
Victor Ayala <vickyflow6@gmail.com>
2023-08-25 16:04:02 -0400

    feat: Crea los elementos html del componente SetTrip

--- front/src/pages/SetTrip.tsx
+++ front/src/pages/SetTrip.tsx
@@ -3,7 +3,19 @@ import { BiArrowBack } from "react-icons/bi"
 const SetTrip = () => {
     return (
         <main>
-            <div><BiArrowBack clasName="text-12" /></div>
+            <h2 className="text-18 flex"><BiArrowBack /> Solicitar un viaje</h2>
+            <form>
+                <ul>
+                    <li><input type="text" placeholder="¿De dónde salís?" /></li>
+                    <li><input type="text" placeholder="¿A dónde te llevamos?" /></li>
+                </ul>
+            </form>
+            <div>
+                <h5>Recientes</h5>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </main>
     );
 }
