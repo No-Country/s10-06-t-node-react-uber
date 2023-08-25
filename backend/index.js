cd53948e1033b8be9967853a24437bf72d6d7391
aguusoleti <agustin_soleti@hotmail.com>
2023-08-25 13:58:20 -0300

    fix Mp

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import server from './src/routes/serverRoute.js';
-
+import paymentRoute from './src/routes/paymentRoutes.js'
 import conductor from './src/routes/conductorRoutes.js'
 import { initDBConnection } from './src/data/dbConnection.js';
 import login from './src/routes/loginRoute.js';
@@ -27,10 +27,10 @@ app.use(
 
 app.use('/', server);
 app.use('/conductor', conductor);
-app.use(express.json());
 app.use('/', login);
 app.use('/', registerUser);
 app.use('/', emailVerification);
+app.use('/payment', paymentRoute );
 
 
 app.listen(PORT, () => {
