8d8d956bd512a8f429c937449b37def3d79d7600
Victor Ayala <vickyflow6@gmail.com>
2023-08-28 15:26:30 -0400

    refactor: Crea componente 'RecentsTripsItem'
    Esté componente, renderiza los item de la sección de viajes recientes y se utiliza en la página de 'SetTrip'.

--- front/src/pages/SetTrip.tsx
+++ front/src/pages/SetTrip.tsx
@@ -1,5 +1,5 @@
 import Container from "../components/layouts/Container" 
-import { BiArrowBack } from "react-icons/bi"
+import { BiArrowBack, BiTime } from "react-icons/bi"
 import { TbPointFilled } from "react-icons/tb"
 import { GoTriangleDown } from "react-icons/go"
 
@@ -7,7 +7,7 @@ const SetTrip: React.FC = () => {
     return (
         <Container>
             <main className="text-20">
-                <h2 className="flex"><BiArrowBack /> Solicitar un viaje</h2>
+                <h2 className="flex text-24 items-center"><BiArrowBack className="text-primary mr-3" /> Solicitar un viaje</h2>
                 <form className="my-7 relative">
                     <div className="flex items-center mb-5">    
                         <TbPointFilled className="text-24 mr-[4px]" />
@@ -20,13 +20,19 @@ const SetTrip: React.FC = () => {
                 </form>
                 <div>
                     <h5>Recientes</h5>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                    <RecentTripsItem finishLocation="Av Colón 3240" km={7} />
+                    <RecentTripsItem finishLocation="Mariano Moreno 106" km={9} />
+                    <RecentTripsItem finishLocation="Arroyo Cabral 8756" km={25} />
                 </div>
             </main>
         </Container>
     );
+
+    function RecentTripsItem({ finishLocation, km }: { finishLocation: string, km: number } ) {
+        return (
+            <div><BiTime /> { finishLocation } <span>{km} km</span></div>
+        );
+    }
 
     function Input({ inputType, inputPlaceholder, className = "" }: { inputType: string, inputPlaceholder: string, className?: string }) {
         const nameClass = `w-full focus:outline-none rounded-full p-[6px] border-solid border-dark border-2 placeholder-dark ${className}`
