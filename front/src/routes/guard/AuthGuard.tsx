06fe68f9285bedb9f776fc30a0761f0a1056aee5
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-08-28 14:52:36 -0500

    refactor(routes): refactor ProtectedRoute to AuthGuard

--- front/src/routes/guard/AuthGuard.tsx
+++ front/src/routes/guard/AuthGuard.tsx
@@ -2,56 +2,52 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthStore } from '@/context/GoogleAuthContext'
 import { useEffect, useState } from 'react'
 
-export const ProtectedRoute = () => {
+import { BASE_URL } from '@/utils/api'
+
+export const AuthGuard: React.FC = () => {
   const { setUser } = useAuthStore()
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const checkLogin = async () => {
+    const checkLogin = async (): Promise<void> => {
       const token = localStorage.getItem('token')
-      if (!token) {
+      if (token !== null) {
         setIsAuthenticated(false)
         setLoading(false)
         return
       }
 
       try {
-        //Validate Token
-
-        const res = await fetch(
-          'https://s10-06-t-node-react-uber-production.up.railway.app/protected-route',
-          {
-            method: 'POST',
-            body: JSON.stringify({
-              token: token,
-            }),
-            headers: {
-              'Content-Type': 'application/json',
-            },
+        const res = await fetch(`${BASE_URL}/api/protected-route`, {
+          method: 'POST',
+          body: JSON.stringify({
+            token,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
           },
-        )
+        })
+
         if (res.status !== 200) {
           setIsAuthenticated(false)
-          return setLoading(false)
+          setLoading(false)
+          return
         }
+
         setIsAuthenticated(true)
         setLoading(false)
 
-        //Get user info
-
-        const userData = await fetch(
-          'https://s10-06-t-node-react-uber-production.up.railway.app/users/id',
-          {
-            method: 'POST',
-            body: JSON.stringify({
-              token: token,
-            }),
-            headers: {
-              'Content-Type': 'application/json',
-            },
+        const userData = await fetch(`${BASE_URL}/users/id`, {
+          method: 'POST',
+          body: JSON.stringify({
+            token,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
           },
-        )
+        })
+
         const userInfo = await userData.json()
         setUser(userInfo)
       } catch (error) {
@@ -59,8 +55,8 @@ export const ProtectedRoute = () => {
         setLoading(false)
       }
     }
-    checkLogin()
-  }, [])
+    void checkLogin()
+  }, [setUser])
 
   if (loading) return <h1>Loading...</h1>
   if (!isAuthenticated && !loading) return <Navigate to='/login' replace />
