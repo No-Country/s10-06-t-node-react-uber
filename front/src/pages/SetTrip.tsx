8a91a215176e303344a97e6d2f9e9b21aae28c34
Victor Ayala <vickyflow6@gmail.com>
2023-08-28 18:17:13 -0400

    feat(SetTrip): Estiliza las recomendaciones de ubicación

--- front/src/pages/SetTrip.tsx
+++ front/src/pages/SetTrip.tsx
@@ -2,7 +2,7 @@ import Container from "../components/layouts/Container"
 import { BiArrowBack, BiTime } from "react-icons/bi"
 import { TbPointFilled } from "react-icons/tb"
 import { GoTriangleDown } from "react-icons/go"
-import { FaLocationDot } from "react-icons/fa"
+import { HiLocationMarker } from "react-icons/hi"
 import type React from "react"
 
 const SetTrip: React.FC = () => {
@@ -20,12 +20,10 @@ const SetTrip: React.FC = () => {
                         <Input inputType="text" inputPlaceholder="¿A dónde te llevamos?" />
                     </div>
                 </form>
-                <div>
-                    <h5>Recientes</h5>
-                    <RecentTripsItem finishLocation="Av Colón 3240" km={7} />
-                    <RecentTripsItem finishLocation="Mariano Moreno 106" km={9} />
-                    <RecentTripsItem finishLocation="Arroyo Cabral 8756" km={25} />
-                </div>
+                <section>
+                    <h5>Resultados</h5>
+                    <LocationAutocompleteItems locationName="Banco Galicia" location="Av Rafael Nuñez 3254, Córdoba" km={3} />
+                </section>
             </main>
         </Container>
     );
@@ -33,10 +31,10 @@ const SetTrip: React.FC = () => {
     function LocationAutocompleteItems({ locationName, location, km } : { locationName: string, location: string, km: number }) {
         return(
             <ItemsContainer>
-                <FaLocationDot className="mr-3 text-24" />
-                <p>
-                    <span className="font-bold">{locationName}</span>
-                    <span>{location}</span>
+                <HiLocationMarker className="mr-3 text-24 text-primary" />
+                <p className="text-ellipsis overflow-hidden font-light">
+                    <span className="font-bold block">{locationName}</span>
+                    <span className="whitespace-nowrap">{location}</span>
                 </p>
                 <span className="font-bold justify-self-end">{km} km</span>
             </ItemsContainer>
@@ -45,7 +43,7 @@ const SetTrip: React.FC = () => {
     // Esté componente, se utiliza para estilizar el contenedor de los items. 
     function ItemsContainer({ children }: { children: React.ReactNode }) {
         return (
-            <div className="rounded-full items-center py-[6px] px-[12px] bg-[#f8f8f8] grid grid-cols-[max-content_minmax(min-content,_max-content)_minmax(max-content,_1fr)] text grid-rows-1 font-light shadow-setTripItems mt-3">
+            <div className="rounded-full items-center py-[6px] px-[12px] bg-[#f8f8f8] grid grid-flow-col grid-rows-[min-content] grid-cols-[max-content_minmax(min-content,_max-content)_minmax(min-content)] font-light shadow-setTripItems mt-3">
                 {children}
             </div>
         );
