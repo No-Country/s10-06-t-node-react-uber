fba1a29b58784fc915990d572895a9479c9b7d2b
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-08-30 12:34:13 -0500

    refactor: simplify Dashboard component code

--- front/src/pages/Dashboard.tsx
+++ front/src/pages/Dashboard.tsx
@@ -1,28 +1,29 @@
-import { type FC, useState } from 'react';
-import { Home } from './Home';
-import { SectionManager } from '@/components/Dashboard/SectionManager';
-import { ActivityHistory } from './ActivityHistory';
-import { Chat } from './Chat';
-import { AccountManager } from './AccountManager';
+import { type FC, useState } from 'react'
+import { Home } from './Home'
+import { SectionManager } from '@/components/Dashboard/SectionManager'
+import { ActivityHistory } from './ActivityHistory'
+import { Chat } from './Chat'
+import { AccountManager } from './AccountManager'
 
 export const Dashboard: FC = () => {
+  const [currentSection, setCurrentSection] = useState<React.ReactNode>(
+    <Home />,
+  )
 
-    const [ currentSection, setCurrentSection ] = useState<React.ReactNode>(<Home />);
+  const handleSection = (nameSection: string): void => {
+    setCurrentSection(sectionComponents[nameSection] ?? <Home />)
+  }
 
-    const sectionComponents: Record<string, React.ReactNode> = {
-        Home: <Home />,
-        ActivityHistory: <ActivityHistory />,
-        Chat: <Chat />,
-        AccountManager: <AccountManager />,
-    };
+  return (
+    <div className='h-screen w-screen'>
+      {currentSection}
+      <SectionManager handleSection={handleSection} />
+    </div>
+  )
+}
 
-    const handleSection = (nameSection: string) => {
-        setCurrentSection(sectionComponents[nameSection] || <Home />);
-    };
-    return (
-        <div className='h-screen w-screen'>
-            {currentSection}
-            <SectionManager handleSection={handleSection}/>
-        </div>
-    );
-};
+const sectionComponents: Record<string, React.ReactNode> = {
+  ActivityHistory: <ActivityHistory />,
+  Chat: <Chat />,
+  AccountManager: <AccountManager />,
+}
