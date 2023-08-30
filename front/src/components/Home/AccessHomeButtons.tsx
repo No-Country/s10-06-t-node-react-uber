a5abbc169ab3eb02b6eaf214f817568fc5418f2d
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-08-30 12:38:51 -0500

    style: refactor AccessHomeButtons component format

--- front/src/components/Home/AccessHomeButtons.tsx
+++ front/src/components/Home/AccessHomeButtons.tsx
@@ -1,26 +1,29 @@
-import { type FC } from 'react';
-import { AiOutlineMenu, AiTwotoneSetting, } from "react-icons/ai";
-import { CiPercent } from "react-icons/ci";
-import { BiCurrentLocation } from "react-icons/bi";
+import { type FC } from 'react'
+import { AiOutlineMenu, AiTwotoneSetting } from 'react-icons/ai'
+import { CiPercent } from 'react-icons/ci'
+import { BiCurrentLocation } from 'react-icons/bi'
 
 interface AccessHomeButtonsProps {
-    icon: string,
-    modal?: string
+  icon: string
+  modal?: string
 }
 
-export const AccessHomeButtons: FC<AccessHomeButtonsProps> = ({ icon, /*modal*/ }) => {
+export const AccessHomeButtons: FC<AccessHomeButtonsProps> = ({
+  icon /* modal */,
+}) => {
+  const iconComponents: Record<string, JSX.Element> = {
+    MENU: <AiOutlineMenu size='25' />,
+    SETTING: <AiTwotoneSetting size='25' />,
+    DISCOUNT: <CiPercent size='25' />,
+    LOCATION: <BiCurrentLocation size='25' />,
+  }
 
-    const iconComponents: { [key: string]: JSX.Element } = {
-        'MENU': <AiOutlineMenu size='25' />,
-        'SETTING': <AiTwotoneSetting size='25'/>,
-        'DISCOUNT': <CiPercent size='25'/>,
-        'LOCATION': <BiCurrentLocation size='25'/>
-    };
-
-    return (
-        <button className='w-[42px] h-[42px] flex items-center border border-[#29103A]
-            justify-center rounded-full bg-slate-300'>
-            {iconComponents[icon]}
-        </button> 
-    );
-};
+  return (
+    <button
+      className='flex h-[42px] w-[42px] items-center justify-center rounded-full
+            border border-[#29103A] bg-slate-300'
+    >
+      {iconComponents[icon]}
+    </button>
+  )
+}
