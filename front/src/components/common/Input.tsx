85bed683ed98c1efeeedce3b37004cb736fa0ae2
Victor Ayala <vickyflow6@gmail.com>
2023-08-31 15:54:01 -0400

    feat(Input): Agrega escuchador del evento 'key down'
    Agrega característica de pasar props 'keyDownEventActive' y 'handlerKeyDownEventActive', que se utiliza para renderizar condicionalmente un input, de acuerdo a si se necesita escuchar el evento 'keyDown' o no.

--- front/src/components/common/Input.tsx
+++ front/src/components/common/Input.tsx
@@ -4,6 +4,8 @@ interface InputProps {
     className?: string
     handler: (event: React.ChangeEvent<HTMLInputElement>) => void
     value: string
+    keyDownEventActive?: boolean
+    handlerKeyDownEvent?: () => void
 }
 
 const Input: React.FC<InputProps> = ({
@@ -12,16 +14,33 @@ const Input: React.FC<InputProps> = ({
     className = '',
     handler,
     value,
+    keyDownEventActive,
+    handlerKeyDownEvent
 }) => {
+    if(keyDownEventActive){
+        return (
+            <input
+                onChange={(event) => {
+                    handler(event)
+                }}
+                onKeyDown={() => {handlerKeyDownEvent ? handlerKeyDownEvent() : console.log("Error")}}
+                value={value}
+                className={`w-full rounded-full border-2 border-solid border-dark px-[12px] py-[6px] placeholder-dark shadow-setTripItems focus:outline-none ${className}`}
+                type={inputType}
+                placeholder={inputPlaceholder}
+            />
+        )    
+    }
+
     return (
         <input
-        onChange={(event) => {
-            handler(event)
-        }}
-        value={value}
-        className={`w-full rounded-full border-2 border-solid border-dark px-[12px] py-[6px] placeholder-dark shadow-setTripItems focus:outline-none ${className}`}
-        type={inputType}
-        placeholder={inputPlaceholder}
+            onChange={(event) => {
+                handler(event)
+            }}
+            value={value}
+            className={`w-full rounded-full border-2 border-solid border-dark px-[12px] py-[6px] placeholder-dark shadow-setTripItems focus:outline-none ${className}`}
+            type={inputType}
+            placeholder={inputPlaceholder}
         />
     )
 }
