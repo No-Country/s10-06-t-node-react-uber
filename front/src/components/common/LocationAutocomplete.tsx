2cee48921fc5572bab8cd8347baeec2b497a65d3
Victor Ayala <vickyflow6@gmail.com>
2023-09-01 18:52:21 -0400

    feat(SetTrip): Crea 'LocationAutocomplete'
    Esté componente, devolverá las posibles ubicaciones que el usuario este buscando.

---
+++ front/src/components/common/LocationAutocomplete.tsx
@@ -0,0 +1,9 @@
+import LocationAutocompleteItems from './LocationAutocompleteItems'
+
+const LocationAutocomplete: React.FC = () => {
+  return (
+    <LocationAutocompleteItems km={5} location='fasd' locationName='fasd' />
+  )
+}
+
+export default LocationAutocomplete
