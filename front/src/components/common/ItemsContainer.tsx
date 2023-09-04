ec2c4f29cbe69caafefc69f98507dea0a47911d3
Victor Ayala <vickyflow6@gmail.com>
2023-09-04 18:29:37 -0400

    feat(ItemsContainer): Agrega prop 'handler'
    Está prop, almacena la función que se disparará al hacer click en el elemento.

--- front/src/components/common/ItemsContainer.tsx
+++ front/src/components/common/ItemsContainer.tsx
@@ -1,11 +1,18 @@
 interface ItemsContainerProps {
   children: React.ReactNode
+  handler?: () => void
 }
 
 // Esté componente, se utiliza para estilizar el contenedor de los items.
-const ItemsContainer: React.FC<ItemsContainerProps> = ({ children }) => {
+const ItemsContainer: React.FC<ItemsContainerProps> = ({
+  children,
+  handler,
+}) => {
   return (
-    <button className='mt-3 flex w-full items-center rounded-full bg-[#f8f8f8] px-[12px] py-[6px] font-light shadow-setTripItems'>
+    <button
+      onClick={handler}
+      className='mt-3 flex w-full items-center rounded-full bg-[#f8f8f8] px-[12px] py-[6px] font-light shadow-setTripItems'
+    >
       {children}
     </button>
   )
