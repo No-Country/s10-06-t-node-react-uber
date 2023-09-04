d21ac7302b12e20b2e7ae1f1aaa5680e1a45ba93
Victor Ayala <vickyflow6@gmail.com>
2023-09-04 15:41:05 -0400

    feat(SetTrip): Crea 'typeLocationIQAutocompleteData'
    Está interfaz, hace referencia al tipo de dato que devuelve la solicitud al endpoint 'https://us1.locationiq.com/v1/autocomplete'

--- front/src/components/common/LocationAutocomplete.tsx
+++ front/src/components/common/LocationAutocomplete.tsx
@@ -1,5 +1,32 @@
 import LocationAutocompleteItems from './LocationAutocompleteItems'
+import { create } from 'zustand'
 
+interface typeLocationIQAutocompleteData {
+  place_id: number,
+  osm_id: number,
+  osm_type: string,
+  licence: string,
+  lat: string,
+  lon: number,
+  boundingbox: Array<number>,
+  class: string,
+  type: string,
+  display_name: string,
+  display_place: string,
+  display_address: string,
+  address: {
+      name: string,
+      house_number: number,
+      road: string,
+      neighbourhood: string,
+      suburb: string,
+      city: string,
+      state: string,
+      postcode: string,
+      country: string,
+      country_code: string
+  }
+}
 interface typePosiblesLocationState {
   posiblesLocation: object[]
   setPosiblesLocation: (newPosiblesLocation: object[]) => void
@@ -12,10 +39,17 @@ const usePosiblesLocationStore = create<typePosiblesLocationState>()((set) => ({
     }))
   },
 }))
+
 const LocationAutocomplete: React.FC = () => {
-  return (
-    <LocationAutocompleteItems km={5} location='fasd' locationName='fasd' />
+  const { posiblesLocation, setPosiblesLocation } = usePosiblesLocationStore(
+    (state) => state,
   )
+
+  posiblesLocation.map((posibleLocation) => {
+    return (
+      <LocationAutocompleteItems key= km={5} location='fasd' locationName='fasd' />
+    )
+  })
 }
 
 export default LocationAutocomplete
