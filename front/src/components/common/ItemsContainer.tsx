b682c9ae31937be7ff0074de5b4c689eb962c0d0
Victor Ayala <vickyflow6@gmail.com>
2023-09-04 17:44:46 -0400

    fix(SetTrip): Soluciona errores de estilo del 'LocationAutocompleteItems'

--- front/src/components/common/ItemsContainer.tsx
+++ front/src/components/common/ItemsContainer.tsx
@@ -5,7 +5,7 @@ interface ItemsContainerProps {
 // Esté componente, se utiliza para estilizar el contenedor de los items.
 const ItemsContainer: React.FC<ItemsContainerProps> = ({ children }) => {
   return (
-    <div className='mt-3 grid grid-flow-col grid-cols-[max-content_minmax(min-content,_max-content)_minmax(min-content)] grid-rows-[min-content] items-center rounded-full bg-[#f8f8f8] px-[12px] py-[6px] font-light shadow-setTripItems'>
+    <div className='mt-3 flex items-center rounded-full bg-[#f8f8f8] px-[12px] py-[6px] font-light shadow-setTripItems'>
       {children}
     </div>
   )
