927c4cd71a5a4f40be545ca9e84a7a57292bfac1
Victor Ayala <vickyflow6@gmail.com>
2023-09-04 18:49:03 -0400

    feat: Crea utilidad 'locationIqAutocomplete.ts
    Con está utilidad, realizaremos las solicitudes al endpoint '/autocomplete' de la api de LocationIq.

---
+++ front/src/utils/locationIqAutocomplete.ts
@@ -0,0 +1,26 @@
+import locationIqAccessToken from './locationIqAccessToken'
+import locationIqApiBaseUrl from './locationIqApi'
+import usePosiblesLocationStore from '@/stateManagement/usePosiblesLocationStore'
+
+function locationIqAutocomplete(): Promise<T> {
+  const { setPosiblesLocation } = usePosiblesLocationStore((state) => state)
+  await fetch(
+    `${locationIqApiBaseUrl}/autocomplete?key=${locationIqAccessToken}&q=${inputFinishLocationValue}`,
+  )
+    .then(async (response) => await response.json())
+    .then((data) => {
+      if (data.error) {
+        console.log('error')
+      } else {
+        return data
+      }
+    })
+    .then((data: typeLocationIQAutocompleteData | undefined) => {
+      setPosiblesLocation(data, 'inputFinishLocation')
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+export default locationIqAutocomplete
