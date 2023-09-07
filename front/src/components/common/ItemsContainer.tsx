f0c9ee3cafb83e87c7c44202030cc144f395bf61
Victor Ayala <vickyflow6@gmail.com>
2023-09-07 13:24:13 -0400

    feat(ItemsContainer): Agrega 'onClick'

--- front/src/components/common/ItemsContainer.tsx
+++ front/src/components/common/ItemsContainer.tsx
@@ -1,11 +1,22 @@
 interface ItemsContainerProps {
   children: React.ReactNode
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 // Esté componente, se utiliza para estilizar el contenedor de los items.
-const ItemsContainer: React.FC<ItemsContainerProps> = ({ children }) => {
+const ItemsContainer: React.FC<ItemsContainerProps> = ({
+  children,
+  onClick,
+}) => {
   return (
-    <button className='mt-3 flex items-center rounded-full bg-[#f8f8f8] px-[12px] py-[6px] font-light shadow-setTripItems'>
+    <button
+      onClick={(event) => {
+        if (onClick) {
+          onClick(event)
+        }
+      }}
+      className='mt-3 flex items-center rounded-full bg-[#f8f8f8] px-[12px] py-[6px] font-light shadow-setTripItems'
+    >
       {children}
     </button>
   )
