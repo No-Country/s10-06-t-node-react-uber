ef66a78eb07b0a9c0f0bc57a54734b9cd629e3d0
Victor Ayala <vickyflow6@gmail.com>
2023-09-07 13:28:13 -0400

    feat(ItemsContainer): Elimina 'onClick'

--- front/src/components/common/ItemsContainer.tsx
+++ front/src/components/common/ItemsContainer.tsx
@@ -2,22 +2,13 @@ import { Link } from 'react-router-dom'
 
 interface ItemsContainerProps {
   children: React.ReactNode
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 // Esté componente, se utiliza para estilizar el contenedor de los items.
-const ItemsContainer: React.FC<ItemsContainerProps> = ({
-  children,
-  onClick,
-}) => {
+const ItemsContainer: React.FC<ItemsContainerProps> = ({ children }) => {
   return (
     <Link
       to='/select-trip'
-      onClick={(event) => {
-        if (onClick) {
-          onClick(event)
-        }
-      }}
       className='mt-3 flex items-center rounded-full bg-[#f8f8f8] px-[12px] py-[6px] font-light shadow-setTripItems'
     >
       {children}
