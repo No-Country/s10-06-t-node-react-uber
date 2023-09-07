3b736a952b4a159826ab6d005590abb7e9cc64e9
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-09-07 00:27:21 -0500

    test: Add tests for firebase config properties

--- front/tests/services/firebase.config.test.ts
+++ front/tests/services/firebase.config.test.ts
@@ -2,26 +2,32 @@ import { firebaseConfig } from '@/services/firebase.config'
 
 describe('Tests on services/firebase.config.ts', () => {
   it('should have a apiKey', () => {
+    expect(firebaseConfig.apiKey).toBeDefined()
     expect(firebaseConfig.apiKey.length).toBeGreaterThan(0)
   })
 
   it('should have a authDomain', () => {
+    expect(firebaseConfig.authDomain).toBeDefined()
     expect(firebaseConfig.authDomain.length).toBeGreaterThan(0)
   })
 
   it('should have a projectId', () => {
+    expect(firebaseConfig.projectId).toBeDefined()
     expect(firebaseConfig.projectId.length).toBeGreaterThan(0)
   })
 
   it('should have a storageBucket', () => {
+    expect(firebaseConfig.storageBucket).toBeDefined()
     expect(firebaseConfig.storageBucket.length).toBeGreaterThan(0)
   })
 
   it('should have a messagingSenderId', () => {
+    expect(firebaseConfig.messagingSenderId).toBeDefined()
     expect(firebaseConfig.messagingSenderId.length).toBeGreaterThan(0)
   })
 
   it('should have a appId', () => {
+    expect(firebaseConfig.appId).toBeDefined()
     expect(firebaseConfig.appId.length).toBeGreaterThan(0)
   })
 })
