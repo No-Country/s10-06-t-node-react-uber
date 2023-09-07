19566dd0fa0136a89e839a5c97c149868230521e
Victor Ayala <vickyflow6@gmail.com>
2023-09-07 13:19:37 -0400

    feat(ItemsContainer): Cambia 'div' a 'button'

--- front/src/components/common/ItemsContainer.tsx
+++ front/src/components/common/ItemsContainer.tsx
@@ -5,9 +5,9 @@ interface ItemsContainerProps {
 // Esté componente, se utiliza para estilizar el contenedor de los items.
 const ItemsContainer: React.FC<ItemsContainerProps> = ({ children }) => {
   return (
-    <div className='mt-3 flex items-center rounded-full bg-[#f8f8f8] px-[12px] py-[6px] font-light shadow-setTripItems'>
+    <button className='mt-3 flex items-center rounded-full bg-[#f8f8f8] px-[12px] py-[6px] font-light shadow-setTripItems'>
       {children}
-    </div>
+    </button>
   )
 }
 
