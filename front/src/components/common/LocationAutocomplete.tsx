ca1909fd137d9e979a61e2270d92309370aa20bd
Victor Ayala <vickyflow6@gmail.com>
2023-09-07 17:16:00 -0400

    fix(LocationAutocomplete): Soluciona error en importación

--- front/src/components/common/LocationAutocomplete.tsx
+++ front/src/components/common/LocationAutocomplete.tsx
@@ -1,5 +1,5 @@
 import LocationAutocompleteItems from './LocationAutocompleteItems'
-import usePosiblesLocationStore from '@/stateManagement/usePosiblesLocationStore'
+import { usePosiblesLocationStore } from '@/stateManagement/usePosiblesLocationStore'
 
 export type typeLocationIQAutocompleteData = Array<{
   place_id: number
