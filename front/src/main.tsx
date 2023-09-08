36edd170c908decf39b6c1503b80e37931562499
Victor Ayala <vickyflow6@gmail.com>
2023-09-08 15:05:12 -0400

    fix(styles): Carga estilos de tailwind

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles/index.css'
 import RoutesProvider from './routes/RoutesProvider'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
